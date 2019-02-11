d328a74ed0b25ec9ec801fabe2ae2abddfe9ad2b
Aljosha <almei105@hhu.de>
2019-02-11 14:47:43 +0100

    add person and item repository

---
+++ ausleih-service/src/main/java/hhu/ausleihservice/dataaccess/PersonRepository.java
@@ -0,0 +1,10 @@
+package hhu.ausleihservice.dataaccess;
+
+import hhu.ausleihservice.databasemodel.Person;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends CrudRepository<Person,Long> {
+	List<Person> findAll();
+}
