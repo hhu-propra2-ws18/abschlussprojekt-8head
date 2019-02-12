c780bb035f295e5d1876a0078f3501cc6848ce0f
burakmartin <burak.martin@mail.de>
2019-02-12 12:40:58 +0100

    added admin.html and config class for authentication

---
+++ ausleih-service/src/main/java/hhu/ausleihservice/web/authentication/config.java
@@ -0,0 +1,20 @@
+package hhu.ausleihservice.web.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class config extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
+    }
+}
