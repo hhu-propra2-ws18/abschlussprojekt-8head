b16ce02b0dfea3f0f06727f5fb68c2f52c656d77
kingfishbrain <romancherkovsky@googlemail.com>
2019-02-13 14:45:19 +0100

    add ProPayService

--- ausleih-service/src/main/java/hhu/ausleihservice/propay/ProPayInterface.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/propay/ProPayInterface.java
@@ -1,11 +1,13 @@
 package hhu.ausleihservice.propay;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class ProPayInterface {
 
 	public ProPayAccount getAccountInfo(String proPayName) {
