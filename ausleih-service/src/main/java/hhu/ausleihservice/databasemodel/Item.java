a4a90a9fba2f674ef10431d2f658d5a005f2dc9e
Jan Kneiphof <jan.kneiphof@uni-duesseldorf.de>
2019-02-15 15:00:16 +0100

    change visibility from getAvailablePeriods()

--- ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/Item.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/Item.java
@@ -81,7 +81,7 @@ public class Item {
 		return true;
 	}
 
-	ArrayList<Period> getAvailablePeriods(){
+	public ArrayList<Period> getAvailablePeriods(){
 
 		ArrayList<Period> out = new ArrayList<>();
 
