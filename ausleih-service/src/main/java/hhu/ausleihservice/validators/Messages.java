044a0592f1da0c5cc920902c718d7f615da61194
Jan Kneiphof <jan.kneiphof@uni-duesseldorf.de>
2019-02-20 13:06:30 +0100

    add Messages.java

---
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/Messages.java
@@ -0,0 +1,13 @@
+package hhu.ausleihservice.validators;
+
+public class Messages {
+	public static String notEmpty = "Benötigtes Feld";
+	public static String usernameSize = "Der Benutzername muss zwischen 6 und 32 Zeichen lang sein.";
+	public static String duplicateUsername = "Benutzername bereits vergeben.";
+	public static String passwordSize = "Passwort muss zwischen 8 und 100 Zeichen lang sein.";
+	public static String invalidRole = "Diese Rolle existiert nicht.";
+	public static String invalidEmail = "Email ist nicht gültig.";
+	public static String sizeLocationDescription = "Beschreibung muss zwischen 6 und 400 Zeichen lang sein.";
+	public static String sizeTitle = "Titel muss zwischen 6 und 40 Zeichen lang sein.";
+	public static String sizeItemDescription = "Beschreibung muss zwischen 6 und 4000 Zeichen lang sein.";
+}
