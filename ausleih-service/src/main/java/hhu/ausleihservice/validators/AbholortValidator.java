3b9842e52759d98bc6add00316e17e9feaadf578
MarvinOh <marvin.ohst@hhu.de>
2019-02-20 11:01:44 +0100

    export error messages into validation.properties

--- ausleih-service/src/main/java/hhu/ausleihservice/validators/AbholortValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/AbholortValidator.java
@@ -1,10 +1,12 @@
 package hhu.ausleihservice.validators;
 
 import hhu.ausleihservice.databasemodel.Abholort;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+@Component
 public class AbholortValidator implements Validator {
 
 	@Override
@@ -18,7 +20,7 @@ public class AbholortValidator implements Validator {
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"beschreibung","NotEmpty");
 		if (abholort.getBeschreibung().length() < 6 || abholort.getBeschreibung().length() > 400) {
-			errors.rejectValue("beschreibung", "Size.abholortForm.beschreibung");
+			errors.rejectValue("beschreibung", "Size.locationForm.description");
 		}
 	}
 }
--- ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
@@ -1,5 +1,6 @@
 package hhu.ausleihservice.validators;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -8,6 +9,7 @@ import hhu.ausleihservice.databasemodel.Person;
 import hhu.ausleihservice.databasemodel.Rolle;
 import hhu.ausleihservice.web.PersonService;
 
+@Component
 public class PersonValidator implements Validator {
 
 	PersonService personService;
@@ -26,22 +28,29 @@ public class PersonValidator implements Validator {
 		Person person = (Person) target;
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
-		if (personService.findByUsername(person.getUsername()) != null) {
-			errors.rejectValue("username", "User schon vorhanden");
+		if (person.getUsername().length() < 6 || person.getUsername().length() > 32) {
+			errors.rejectValue("username", "Size.personForm.username");
 		}
+		if (personService.findByUsername(person.getUsername()).isPresent()) {
+			errors.rejectValue("username", "Duplicate.personForm.username");
+		}
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
-		if (person.getPassword().length() < 8 || person.getPassword().length() >= 255) {
-			errors.rejectValue("password", "Passwort muss mehr als 8 und weniger als 255 Zeichen beinhalten");
+		if (person.getPassword().length() < 8 || person.getPassword().length() > 100) {
+			errors.rejectValue("password", "Size.personForm.password");
 		}
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "rolle", "NotEmpty");
 		if(person.getRolle() != Rolle.ADMIN && person.getRolle() != Rolle.USER) {
-			errors.rejectValue("rolle", "Diese Rolle existiert nicht");
+			errors.rejectValue("rolle", "Invalid.personForm.role");
 		}
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty");
 		//Email format Regular Expression from RFC 2822
 		if(!person.getEmail().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
-			errors.rejectValue("email", "Email ist nicht gueltig");
+			errors.rejectValue("email", "Invalid.personForm.email");
 		}
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "vorname", "NotEmpty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nachname", "NotEmpty");
 	}
