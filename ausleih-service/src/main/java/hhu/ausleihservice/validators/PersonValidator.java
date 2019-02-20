a3a2d735db7a5bcdec8fb7db0257ce8fdbb631a5
Jan Kneiphof <jan.kneiphof@uni-duesseldorf.de>
2019-02-20 11:22:38 +0100

    add null check on parameter email

--- ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
@@ -36,10 +36,13 @@ public class PersonValidator implements Validator {
 			errors.rejectValue("password", "Passwort muss mehr als 8 und weniger als 255 Zeichen beinhalten");
 		}
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty");
-		//Email format Regular Expression from RFC 2822
-//		if(!person.getEmail().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
-//			errors.rejectValue("email", "Email ist nicht gueltig");
-//		}
+		// Email format Regular Expression from RFC 2822
+		if (!(person.getEmail() == null)) {
+			if (!person.getEmail().matches(
+					"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+				errors.rejectValue("email", "Email ist nicht gueltig");
+			}
+		}
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "vorname", "NotEmpty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nachname", "NotEmpty");
 	}
