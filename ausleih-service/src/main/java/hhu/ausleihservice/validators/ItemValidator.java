3b152cfcbf1c12eb263b33bddee5e3901ee68a2a
MarvinOh <marvin.ohst@hhu.de>
2019-02-20 10:14:13 +0100

    add validators for abholort, item, remove editValidators

---
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/ItemValidator.java
@@ -0,0 +1,30 @@
+package hhu.ausleihservice.validators;
+
+import hhu.ausleihservice.databasemodel.Item;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class ItemValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Item.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Item item = (Item) target;
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"titel","NotEmpty");
+		if (item.getTitel().length() < 6 || item.getTitel().length() > 40) {
+			errors.rejectValue("titel", "Size.itemForm.titel");
+		}
+
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"beschreibung","NotEmpty");
+		if (item.getBeschreibung().length() < 6 || item.getBeschreibung().length() > 4000) {
+			errors.rejectValue("beschreibung", "Size.itemForm.beschreibung");
+		}
+	}
+}
--- ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/PersonValidator.java
@@ -1,4 +1,4 @@
 package hhu.ausleihservice.validators;
 
-public class AddItemValidator {
+public class PersonValidator {
 }
