527081a129a9d05a08d95c78a766d63e905aef65
MarvinOh <marvin.ohst@hhu.de>
2019-02-20 14:56:30 +0100

    add latitude and longitude to AbholortValidator

--- ausleih-service/src/main/java/hhu/ausleihservice/validators/Messages.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/Messages.java
@@ -2,12 +2,17 @@ package hhu.ausleihservice.validators;
 
 class Messages {
 	static String notEmpty = "Benötigtes Feld";
+
 	static String usernameSize = "Der Benutzername muss zwischen 6 und 32 Zeichen lang sein.";
 	static String duplicateUsername = "Benutzername bereits vergeben.";
 	static String passwordSize = "Passwort muss zwischen 8 und 100 Zeichen lang sein.";
 	static String invalidRole = "Diese Rolle existiert nicht.";
 	static String invalidEmail = "Email ist nicht gültig.";
+
+	static String latitudeOutOfBounds = "Breitengrad muss zwischen -90 und 90 sein.";
+	static String longitudeOutOfBounds = "Längengrad muss zwischen -180 und 180 sein.";
 	static String sizeLocationDescription = "Beschreibung muss zwischen 6 und 400 Zeichen lang sein.";
+
 	static String sizeTitle = "Titel muss zwischen 6 und 40 Zeichen lang sein.";
 	static String sizeItemDescription = "Beschreibung muss zwischen 6 und 4000 Zeichen lang sein.";
 	static String negativeValue = "Der Wert muss mindestens 1 betragen";
