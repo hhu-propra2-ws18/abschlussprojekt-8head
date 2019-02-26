a82449a6b6d5c5ad5bf526befc804048f8d90e2b
MarvinOh <marvin.ohst@hhu.de>
2019-02-26 13:42:54 +0100

    remove unused imports

--- ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/AusleihItem.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/AusleihItem.java
@@ -10,9 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
