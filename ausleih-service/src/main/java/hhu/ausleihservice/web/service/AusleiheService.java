3dbf66bd327364c5ae595b49c607e4b14883f4c3
zn5 <jakob.kalus@hhu.de>
2019-02-26 09:58:15 +0100

    Add initial scheduled method to update each Ausleihen status

--- ausleih-service/src/main/java/hhu/ausleihservice/web/service/AusleiheService.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/web/service/AusleiheService.java
@@ -2,7 +2,12 @@ package hhu.ausleihservice.web.service;
 
 import hhu.ausleihservice.dataaccess.AusleiheRepository;
 import hhu.ausleihservice.databasemodel.Ausleihe;
+import hhu.ausleihservice.databasemodel.Status;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AusleiheService {
@@ -15,6 +20,26 @@ public class AusleiheService {
 
 	public void save(Ausleihe ausleihe) {
 		ausleiheRepository.save(ausleihe);
+	}
+
+	public List<Ausleihe> findAll() {
+		return ausleiheRepository.findAll();
+	}
+
+	@Scheduled(cron = "0 0 1 * * ?")
+	public void updateAllAusleihenDaily() {
+		LocalDate currnetDate = LocalDate.now();
+
+		System.out.println("Triggering update");
+		for (Ausleihe ausleihe : findAll()) {
+			checkAusleiheDate(ausleihe, currnetDate);
+		}
+	}
+
+	private void checkAusleiheDate(Ausleihe ausleihe, LocalDate date) {
+		if (ausleihe.getStatus().equals(Status.AUSGELIEHEN) && ausleihe.getEndDatum().isBefore(date)) {
+			ausleihe.setStatus(Status.RUECKGABE_VERPASST);
+		}
 	}
 
 }
