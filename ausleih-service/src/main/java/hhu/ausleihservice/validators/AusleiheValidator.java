1edeb064bf76a5713242c0bfed0649c58ae7241c
Jan Kneiphof <jan.kneiphof@uni-duesseldorf.de>
2019-02-26 13:46:12 +0100

    add error for not Enough Money

--- ausleih-service/src/main/java/hhu/ausleihservice/validators/AusleiheValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/AusleiheValidator.java
@@ -8,14 +8,17 @@ import org.springframework.validation.Validator;
 import hhu.ausleihservice.databasemodel.Ausleihe;
 import hhu.ausleihservice.databasemodel.Item;
 import hhu.ausleihservice.web.service.ItemAvailabilityService;
+import hhu.ausleihservice.web.service.ProPayService;
 
 @Component
 public class AusleiheValidator implements Validator {
 
 	private ItemAvailabilityService availabilityService;
+	private ProPayService proPayService;
 
-	public AusleiheValidator(ItemAvailabilityService availabilityService) {
+	public AusleiheValidator(ItemAvailabilityService availabilityService, ProPayService proPayService) {
 		this.availabilityService = availabilityService;
+		this.proPayService = proPayService;
 	}
 
 	@Override
@@ -40,6 +43,14 @@ public class AusleiheValidator implements Validator {
 		if (ausleiheItem != null && ausleihe.getAusleiher() != null) {
 			if (ausleihe.getAusleiher().equals(ausleiheItem.getBesitzer())) {
 				errors.rejectValue("ausleiher", Messages.ownItemAusleihe);
+			}
+		}
+		
+		if(ausleihe.getAusleiher() != null && ausleiheItem.getBesitzer() != null) {
+			double kontostand = proPayService.getProPayKontostand(ausleihe.getAusleiher());
+			int kautionswert = ausleiheItem.getKautionswert();
+			if(kontostand < kautionswert) {
+				errors.rejectValue("ausleiher", Messages.notEnoughMoney);
 			}
 		}
 	}
