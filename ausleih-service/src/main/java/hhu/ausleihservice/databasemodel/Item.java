17cf2e8473ec86a0f01599f0300359710b642f73
MarvinOh <marvin.ohst@hhu.de>
2019-02-26 09:54:35 +0100

    remove getAvailablePeriods and getSortierteAusleihen again

--- ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/Item.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/databasemodel/Item.java
@@ -166,69 +166,6 @@ public class Item {
 		return this.getPicture100() != null ? Base64.getEncoder().encodeToString(this.getPicture100()) : null;
 	}
 
-	public ArrayList<Period> getAvailablePeriods() {
-
-		ArrayList<Period> out = new ArrayList<>();
-
-		if (ausleihen.isEmpty()) {
-			out.add(new Period(availableFrom, availableTill));
-			return out;
-		}
-
-		Ausleihe[] sortierteAusleihen = getSortierteAusleihen();
-		int length = sortierteAusleihen.length;
-
-		LocalDate start = availableFrom;
-		LocalDate end = sortierteAusleihen[0].getStartDatum();
-
-		if (!start.equals(end)) {
-			out.add(new Period(start, end.minusDays(1)));
-		}
-
-		for (int i = 0; i < length - 1; i++) {
-
-			start = sortierteAusleihen[i].getEndDatum();
-			end = sortierteAusleihen[i + 1].getStartDatum();
-
-			if (!start.equals(end)) {
-				out.add(new Period(start.plusDays(1), end.minusDays(1)));
-			}
-		}
-
-		start = sortierteAusleihen[length - 1].getEndDatum();
-		end = availableTill;
-
-		if (!start.equals(end)) {
-			out.add(new Period(start.plusDays(1), end));
-		}
-
-		return out;
-	}
-
-	Ausleihe[] getSortierteAusleihen() {
-
-		Ausleihe[] sortierteAusleihen = new Ausleihe[ausleihen.size()];
-		List<Ausleihe> tempAusleihen = new ArrayList<>(ausleihen);
-
-		for (int i = 0; i < ausleihen.size(); i++) {
-
-			Ausleihe smallest = tempAusleihen.get(0);
-			LocalDate smallestDate = smallest.getStartDatum();
-
-			for (Ausleihe test : tempAusleihen) {
-				LocalDate testDate = test.getStartDatum();
-				if (testDate.isBefore(smallestDate)) {
-					smallest = test;
-					smallestDate = smallest.getStartDatum();
-				}
-			}
-			sortierteAusleihen[i] = smallest;
-			tempAusleihen.remove(smallest);
-		}
-
-		return sortierteAusleihen;
-	}
-
 	public void setTitel(String s) {
 		if (s != null) {
 			titel = s.trim();
