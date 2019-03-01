260687ef869af248c7d7f8b47a9890ccfa1a367c
Aljosha <almei105@hhu.de>
2019-03-01 13:31:53 +0100

    Extract Methods in Validators

--- ausleih-service/src/main/java/hhu/ausleihservice/validators/AusleihItemValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/AusleihItemValidator.java
@@ -20,16 +20,57 @@ public class AusleihItemValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		AusleihItem item = (AusleihItem) target;
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titel", Messages.notEmpty);
-		if (item.getTitel().length() < 4 || item.getTitel().length() > 40) {
-			errors.rejectValue("titel", Messages.sizeTitle);
-		}
+		validateTitel(item, errors);
+		validateBeschreibung(item, errors);
+		validateKaufItem(item, errors);
+		validateTagessatz(item, errors);
+		validateAvailableFromAndAvailableTill(item, errors);
+		validateAbholort(errors, item);
+	}
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "beschreibung", Messages.notEmpty);
-		if (item.getBeschreibung().length() < 4 || item.getBeschreibung().length() > 4000) {
-			errors.rejectValue("beschreibung", Messages.sizeItemDescription);
+	private void validateAbholort(Errors errors, AusleihItem item) {
+		if (item.getAbholort() == null) {
+			ValidationUtils.rejectIfEmpty(errors, "abholort", Messages.notEmpty);
 		}
+	}
 
+	private void validateAvailableFromAndAvailableTill(AusleihItem item, Errors errors) {
+		if (item.getAvailableFrom() != null && item.getAvailableTill() != null) {
+			validateInvalidPeriod(item, errors);
+			validateInvalidAvailableFrom(item, errors);
+		} else {
+			errors.rejectValue("availableFrom", Messages.notEmpty);
+			errors.rejectValue("availableTill", Messages.notEmpty);
+		}
+	}
+
+	private void validateInvalidAvailableFrom(AusleihItem item, Errors errors) {
+		if (item.getAvailableFrom().isBefore(LocalDate.now())) {
+			errors.rejectValue("availableFrom", Messages.invalidAvailableFrom);
+		}
+	}
+
+	private void validateInvalidPeriod(AusleihItem item, Errors errors) {
+		if (item.getAvailableFrom().isAfter(item.getAvailableTill())) {
+			errors.rejectValue("availableFrom", Messages.invalidPeriod);
+		}
+	}
+
+	private void validateTagessatz(AusleihItem item, Errors errors) {
+		if (item.getTagessatz() != null) {
+			validateNegativeTagessatz(item, errors);
+		} else {
+			errors.rejectValue("tagessatz", Messages.notEmpty);
+		}
+	}
+
+	private void validateNegativeTagessatz(AusleihItem item, Errors errors) {
+		if (item.getTagessatz() <= 0) {
+			errors.rejectValue("tagessatz", Messages.negativeValue);
+		}
+	}
+
+	private void validateKaufItem(AusleihItem item, Errors errors) {
 		if (item.getKautionswert() != null) {
 			if (item.getKautionswert() <= 0) {
 				errors.rejectValue("kautionswert", Messages.negativeValue);
@@ -37,30 +78,19 @@ public class AusleihItemValidator implements Validator {
 		} else {
 			errors.rejectValue("kautionswert", Messages.notEmpty);
 		}
+	}
 
-		if (item.getTagessatz() != null) {
-
-			if (item.getTagessatz() <= 0) {
-				errors.rejectValue("tagessatz", Messages.negativeValue);
-			}
-		} else {
-			errors.rejectValue("tagessatz", Messages.notEmpty);
+	private void validateBeschreibung(AusleihItem item, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "beschreibung", Messages.notEmpty);
+		if (item.getBeschreibung().length() < 4 || item.getBeschreibung().length() > 4000) {
+			errors.rejectValue("beschreibung", Messages.sizeItemDescription);
 		}
+	}
 
-		if (item.getAvailableFrom() != null && item.getAvailableTill() != null) {
-			if (item.getAvailableFrom().isAfter(item.getAvailableTill())) {
-				errors.rejectValue("availableFrom", Messages.invalidPeriod);
-			}
-			if (item.getAvailableFrom().isBefore(LocalDate.now())) {
-				errors.rejectValue("availableFrom", Messages.invalidAvailableFrom);
-			}
-		} else {
-			errors.rejectValue("availableFrom", Messages.notEmpty);
-			errors.rejectValue("availableTill", Messages.notEmpty);
-		}
-
-		if (item.getAbholort() == null) {
-			ValidationUtils.rejectIfEmpty(errors, "abholort", Messages.notEmpty);
+	private void validateTitel(AusleihItem item, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titel", Messages.notEmpty);
+		if (item.getTitel().length() < 4 || item.getTitel().length() > 40) {
+			errors.rejectValue("titel", Messages.sizeTitle);
 		}
 	}
 }
--- ausleih-service/src/main/java/hhu/ausleihservice/validators/KaufItemValidator.java
+++ ausleih-service/src/main/java/hhu/ausleihservice/validators/KaufItemValidator.java
@@ -19,26 +19,43 @@ public class KaufItemValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		KaufItem item = (KaufItem) target;
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titel", Messages.notEmpty);
-		if (item.getTitel().length() < 4 || item.getTitel().length() > 40) {
-			errors.rejectValue("titel", Messages.sizeTitle);
-		}
+		validateTitel(item, errors);
+		validateBeschreibung(item, errors);
+		validateKaufpreis(item, errors);
+		validateAbholort(item, errors);
+	}
 
+	private void validateAbholort(KaufItem item, Errors errors) {
+		if (item.getAbholort() == null) {
+			ValidationUtils.rejectIfEmpty(errors, "abholort", Messages.notEmpty);
+		}
+	}
+
+	private void validateKaufpreis(KaufItem item, Errors errors) {
+		if (item.getKaufpreis() != null) {
+			validateNegativeTagessatz(item, errors);
+		} else {
+			errors.rejectValue("kaufpreis", Messages.notEmpty);
+		}
+	}
+
+	private void validateNegativeTagessatz(KaufItem item, Errors errors) {
+		if (item.getKaufpreis() <= 0) {
+			errors.rejectValue("kaufpreis", Messages.negativeValue);
+		}
+	}
+
+	private void validateBeschreibung(KaufItem item, Errors errors) {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "beschreibung", Messages.notEmpty);
 		if (item.getBeschreibung().length() < 4 || item.getBeschreibung().length() > 4000) {
 			errors.rejectValue("beschreibung", Messages.sizeItemDescription);
 		}
+	}
 
-		if (item.getKaufpreis() != null) {
-			if (item.getKaufpreis() <= 0) {
-				errors.rejectValue("kaufpreis", Messages.negativeValue);
-			}
-		} else {
-			errors.rejectValue("kaufpreis", Messages.notEmpty);
-		}
-
-		if (item.getAbholort() == null) {
-			ValidationUtils.rejectIfEmpty(errors, "abholort", Messages.notEmpty);
+	private void validateTitel(KaufItem item, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titel", Messages.notEmpty);
+		if (item.getTitel().length() < 4 || item.getTitel().length() > 40) {
+			errors.rejectValue("titel", Messages.sizeTitle);
 		}
 	}
 }
